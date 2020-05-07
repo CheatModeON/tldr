8293171e1e19e99b58e7a79b4d0c3c4e13138f39
Achilles <achipap@hotmail.com>
2020-05-08 01:27:54 +0200

    Update application.py

--- application.py
+++ application.py
@@ -27,59 +27,59 @@ model = BartForConditionalGeneration.from_pretrained('bart-large-cnn')
 results = []
 id = 0
 
-def threaded_task(duration):
-    for i in range(duration):
-        print("Working... {}/{}".format(i + 1, duration))
-        time.sleep(1)
-        
-def submit(LONG_TEXT, id):
+def submit(LONG_TEXT, id, sent_no, max_words):
     article_input_ids = tokenizer.batch_encode_plus([LONG_TEXT], return_tensors='pt', max_length=1024)['input_ids'].to(torch_device)
     summary_ids = model.generate(input_ids=article_input_ids,
                                  num_beams=4,
                                  length_penalty=2.0,
-                                 max_length=142,
+                                 max_length=max_words,
                                  #min_len=56,
                                  no_repeat_ngram_size=3)
 
+    #summary_ids = model.generate(input_ids=article_input_ids, num_beams=5, num_return_sequences=1, temperature=1.5)
+    #outputs = model.generate(input_ids=article_input_ids, num_beams=5, num_return_sequences=3, temperature=1.5)
+    #summary_txt =""
+    #for i in range(3): #  3 output sequences were generated
+    #    print('Generated {}: {}'.format(i, tokenizer.decode(outputs[i], skip_special_tokens=True)))
+    #    summary_txt += tokenizer.decode(outputs[i], skip_special_tokens=True)
+    
     summary_txt = tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
     global results
     del results[id-1];
     results.insert(id-1, summary_txt);
-
-@app.route("/test")
-def index():
-    #thread = Thread(target=threaded_task, args=(duration,))
-    thread = Thread(target=threaded_task, args=(5,))
-    thread.daemon = True
-    thread.start()
-    return jsonify({'thread_name': str(thread.name),
-                    'started': True})
-                    
-@app.route('/hi/')
+          
+@app.route('/')
 def about():
-    return "hi" #render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/tldr/', methods=["GET"])
+@app.route('/tldr/', methods=["POST"])
 def tldr():
-
-    parameter = request.args.get('txt')
+    parameter = request.form['txt']
+    sent_no = int(request.form['sent_no'])
+    max_words = int(request.form['max_words'])
+    #parameter = request.args.get('txt')
+    #sent_no = 3
+    #max_words = 142
         
     LONG_TEXT = parameter.replace('\n','')
     global id
     global results
-    id = id+1
+    id = id + 1
     results.append("processing...") # waiting for thread
     #submit(LONG_TEXT,id)
     
-    thread = Thread(target=submit, args=(LONG_TEXT,id,))
+    thread = Thread(target=submit, args=(LONG_TEXT,id,sent_no,max_words,))
     thread.daemon = True
     thread.start()
-    return "your token is " +str(id-1);
+    return "your token is " + str(id-1)
 
-@app.route('/tkn/', methods=["GET"])
+@app.route('/tkn', methods=["GET"])
 def token():
-    parameter = request.args.get('token')
-    return results[int(parameter)]
+    parameter = int(request.args['token'])
+    if(parameter < 0 or parameter >= len(results)):
+        return "Invalid Token"
+    else:
+        return results[int(parameter)]
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
