0122655b3a6d3aa13c0b3ef22f6e47bbee550a7e
Achilles <achipap@hotmail.com>
2020-05-08 23:20:43 +0200

    Update application.py

--- application.py
+++ application.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from flask import jsonify
-from flask import request
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 from threading import Thread
 import time
+import os
+from os.path import join, dirname, realpath
 
 from datetime import datetime
 import json
@@ -15,7 +17,15 @@ import torch
 import transformers
 from transformers import BartTokenizer, BartForConditionalGeneration
 
+import pytesseract
+import cv2
+import numpy as np
+
+UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/uploads/')
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 #app.config['JSON_AS_ASCII'] = False
 
@@ -27,12 +37,16 @@ model = BartForConditionalGeneration.from_pretrained('bart-large-cnn')
 results = []
 id = 0
 
-def submit(LONG_TEXT, id, sent_no, max_words):
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def submit(LONG_TEXT, id, max_words):
     article_input_ids = tokenizer.batch_encode_plus([LONG_TEXT], return_tensors='pt', max_length=1024)['input_ids'].to(torch_device)
     summary_ids = model.generate(input_ids=article_input_ids,
                                  num_beams=4,
                                  length_penalty=2.0,
-                                 max_length=max_words,
+                                 max_length=142,
                                  #min_len=56,
                                  no_repeat_ngram_size=3)
 
@@ -44,42 +58,77 @@ def submit(LONG_TEXT, id, sent_no, max_words):
     #    summary_txt += tokenizer.decode(outputs[i], skip_special_tokens=True)
     
     summary_txt = tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
+   
     global results
     del results[id-1];
     results.insert(id-1, summary_txt);
-          
+         
+
 @app.route('/')
 def about():
     return render_template('index.html')
 
+    
+@app.route('/uploader/', methods=["POST"])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        #imPath = str(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        config = ('-l eng --oem 1 --psm 3')
+
+        #im = cv2.imread(imPath, cv2.IMREAD_C.OLOR)
+        
+        filestr = request.files['file'].read()
+        npimg = np.fromstring(filestr, np.uint8)
+        im = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+        text = pytesseract.image_to_string(im, config=config)
+        print(text)
+        return render_template('index.html', txt1=text)
+        #print(url_for('upload_file',filename=filename))
+
 @app.route('/tldr/', methods=["POST"])
 def tldr():
-    parameter = request.form['txt']
-    sent_no = int(request.form['sent_no'])
-    max_words = int(request.form['max_words'])
+    parameter = str(request.form['txt'])
+    #sent_no = int(request.form['sent_no'])
+    #max_words = int(request.form['max_words'])
     #parameter = request.args.get('txt')
+    
     #sent_no = 3
-    #max_words = 142
-        
-    LONG_TEXT = parameter.replace('\n','')
+    max_words = 142
+    LONG_TEXT = parameter#.replace('\n',' ') # replace('\n', ' ') not working correct
+    LONG_TEXT = " ".join(parameter.split())
+
     global id
     global results
     id = id + 1
     results.append("processing...") # waiting for thread
     #submit(LONG_TEXT,id)
     
-    thread = Thread(target=submit, args=(LONG_TEXT,id,sent_no,max_words,))
+    thread = Thread(target=submit, args=(LONG_TEXT,id,max_words,))
     thread.daemon = True
     thread.start()
-    return "your token is " + str(id-1)
+    return "your token is " + str(id-1) + "<br> use it <a href=\"http://localhost:5000/tkn?token="+ str(id-1) +"\">here</a>"
 
 @app.route('/tkn', methods=["GET"])
 def token():
     parameter = int(request.args['token'])
     if(parameter < 0 or parameter >= len(results)):
-        return "Invalid Token"
+        return render_template('token.html', res="Invalid Token")
     else:
-        return results[int(parameter)]
+        return render_template('token.html', res=results[int(parameter)])
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
