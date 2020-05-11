03f980e7dfd9b31152c10edd7d9ae114b9f72f08
Achilleas Papakonstantinou <achilles@Achilleass-MacBook-Pro.local>
2020-05-11 22:36:07 +0200

    bs

--- application.py
+++ application.py
@@ -158,10 +158,8 @@ def get_articles():
         return articles[int(parameter)]
 
 if __name__ == '__main__':
-    app.secret_key = 'super secret key'
-    app.config['SESSION_TYPE'] = 'filesystem'
-
-    sess.init_app(app)
-
-    app.debug = True
+    #app.secret_key = 'super secret key'
+    #app.config['SESSION_TYPE'] = 'filesystem'
+    #sess.init_app(app)
+    #app.debug = True
     app.run(threaded=True, host=HOSTNAME, port=5000)
